111bc2f94324e5fc20fa195860ffb97817c18c8b
mammo0 <marc.ammon@hotmail.de>
2020-10-14 09:15:50 +0200

    added tabs everywhere
    -> just for better readability

--- dotenv.sh
+++ dotenv.sh
@@ -56,9 +56,9 @@ export_envs() {
 			continue
 		fi
 
-        value=$(eval echo "$temp")
-        eval export "$key='$value'";
-		echo "::set-env name=$key::$value"
+		value=$(eval echo "$temp")
+		eval export "$key='$value'";
+			echo "::set-env name=$key::$value"
 	done < $1
 }
 
